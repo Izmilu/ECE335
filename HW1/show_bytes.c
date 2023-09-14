ac913f9ae9b9e68bfe9ea88888b8592c3b986375
izmilu <imilla2@unm.edu>
2023-09-13 18:46:21 -0600

    Homework 1 Source File

---
+++ HW1/show_bytes.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef unsigned char *byte_pointer;
+
+void show_bytes(byte_pointer a, int len) {
+int i;
+for(i=0; i<len; i++) {
+printf("%.2x ", a[i]);
+}
+printf("\n");
+}
+
+void show_int(int a) {
+show_bytes((byte_pointer)&a, sizeof(a));
+}
+
+void show_float(float a) {
+show_bytes((byte_pointer)&a, sizeof(a));
+}
+
+int main(int argv, char** argc) {
+int a = atoi(argc[1]);
+int b = atoi(argc[2]);
+float c = atof(argc[3]);
+printf("show int %d\n", a);
+show_int(a);
+printf("show int %d\n", b);
+show_int(b);
+printf("show float %g\n", c);
+show_float(c);
+return 0;
+}
+
+
